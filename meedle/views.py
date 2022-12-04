d89ef2944c6bea490cb4f960ef15d58418e7a0a4
Annisa Dian <nugrahani07@gmail.com>
2022-12-04 23:08:18 +0700

    revert

--- meedle/views.py
+++ meedle/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
-from bsbi.views import BSBIIndex, VBEPostings, IdMap
+# from bsbi.views import BSBIIndex, VBEPostings, IdMap
 import os
 from poll.settings import BASE_DIR, STATIC_URL,STATIC_ROOT, STATICFILES_DIRS
 from django.core.files import File
@@ -31,13 +31,13 @@ def search_query(request):
 
     query = body["query"]
 
-    BSBI_instance = BSBIIndex(data_dir = 'collection', \
-        postings_encoding = VBEPostings, \
-        output_dir = 'index')
+    # BSBI_instance = BSBIIndex(data_dir = 'collection', \
+    #     postings_encoding = VBEPostings, \
+    #     output_dir = 'index')
 
     docs = []
-    for (_, doc) in BSBI_instance.retrieve_bm25(query, k = 10):
-        docs.append(doc)
+    # for (_, doc) in BSBI_instance.retrieve_bm25(query, k = 10):
+    #     docs.append(doc)
     
     response = {
         "query": query,
--- poll/settings.py
+++ poll/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bsbi',
     'meedle',
     'questions',
       'rest_framework',
